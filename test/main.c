dfda38d551255b5863c288ebf3ba48bcfe291db2
yamana113 <mathisfabre2@gmail.com>
2021-04-15 17:08:48 +0200

    modif de main test pour accomoder test harness

--- test/main.c
+++ test/main.c
@@ -1,4 +1,15 @@
+#include "test_harness/test_harness.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Valeurs pour le harnais de test spécifiques à ce programme.
+// augmenter cette val à chaque test créer
+int const tests_total = 0;
+
 int main()
 {
-    return 0;
+    return tests_executed - tests_successful;
 }
