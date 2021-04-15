720bfeb3dd08fa6600ac30e8aace18c3bda0ee3a
“Nathan <nathang.barbier@gmail.com>
2021-04-15 19:01:06 +0200

    Implémentation des types restaurant, menu, livreur, client sous forme de struct

--- lib/db.h
+++ lib/db.h
@@ -7,33 +7,54 @@
 #include <stdio.h>
 
 #define TAILLE_CHAMP_NOM 36
+#define TAILLE_CHAMP_TEL 14
 
-// Peut être combiné. P. ex. 0xA = DERMATOLOGIE + MEDECINE_GENERALE.
-enum specialite_t
+typedef struct restaurant
 {
-    CARDIOLOGIE         = 0x1,
-    DERMATOLOGIE        = 0x2,
-    ENDOCRINOLOGIE      = 0x4,
-    MEDECINE_GENERALE   = 0x8,
-    OPHTALMOLOGIE       = 0x10,
-    PEDIATRIE           = 0x20,
-};
-
-typedef struct docteur
-{
-    size_t index;   // Clé primaire.
+    size_t id; // Clé primaire.
     char nom[TAILLE_CHAMP_NOM];
-    int specialites;
-    size_t index_hopital;   // Clé secondaire.
-} docteur;
+    int code_postal;
+    char telephone[TAILLE_CHAMP_TEL];
+    char type[TAILLE_CHAMP_NOM];
+    vector menu;
+    float solde;
+} restaurant;
 
-vector lecture_table(
-    FILE* fichier);
+typedef struct menu
+{
+    size_t id; // Clé primaire.
+    char nom[TAILLE_CHAMP_NOM];
+    vector ingredients;
+    float prix;
+} menu;
+
+typedef struct livreur
+{
+    size_t id; // Clé primaire.
+    char nom[TAILLE_CHAMP_NOM];
+    char telephone[TAILLE_CHAMP_TEL];
+    vector deplacements; // Liste de code_postal
+    size_t restaurant;
+    float solde;
+} livreur;
+
+typedef struct client
+{
+    size_t id; // Clé primaire.
+    char nom[TAILLE_CHAMP_NOM];
+    int code_postal;
+    char telephone[TAILLE_CHAMP_TEL];
+    float solde;
+}
+
+vector
+lecture_table(
+    FILE *fichier);
 
 void ecriture_table(
-    FILE* fichier,
-    vector const* db);
+    FILE *fichier,
+    vector const *db);
 
 // Renvoie 'vrai' si le docteur compte 'MEDECINE_GENERALE' parmi ses spécialités.
 bool est_generaliste(
-    void const* d);
+    void const *d);
