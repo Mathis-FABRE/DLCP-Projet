4005099de38af037ce7223eae89395c9845ae8e6
yamana113 <mathisfabre2@gmail.com>
2021-04-21 11:03:38 +0200

    ajout commentaires

--- lib/gestionfichier.h
+++ lib/gestionfichier.h
@@ -27,12 +27,27 @@ vector lecture_livreur(const char * file);
 // met les données dans un vecteur
 vector lecture_menu(const char * file);
 
+// fonction prennant le début et la fin d'un vecteur
+// supposé trié de Menu
+// le met dans le fichier db_menus.csv
 void sauvegarde_clients(iterator first, iterator last);
 
+// prend le pointeur d'un fichier en écriture
+// le début et la fin d'un vecteur
+// sauvegarde ce vecteur dans la ligne sous forme de .;.;.
 void sauvegarde_liste(FILE* file, iterator first, iterator last);
 
+// fonction prennant le début et la fin d'un vecteur
+// supposé trié de Menu
+// le met dans le fichier db_menus.csv
 void sauvegarde_resto(iterator first, iterator last);
 
+// fonction prennant le début et la fin d'un vecteur
+// supposé trié de Menu
+// le met dans le fichier db_menus.csv
 void sauvegarde_livreurs(iterator first, iterator last);
 
+// fonction prennant le début et la fin d'un vecteur
+// supposé trié de Menu
+// le met dans le fichier db_menus.csv
 void sauvegarde_menus(iterator first, iterator last);
