27dd2c1d0d263d69f186b26f46904c672a03ba0f
yamana113 <mathisfabre2@gmail.com>
2021-05-02 18:42:10 +0200

    ajout tests pour fonctions tests et ajout_resto

--- test/main.c
+++ test/main.c
@@ -1,6 +1,7 @@
 #include "gestionfichier.h"
 #include "db.h"
 #include "algorithm.h"
+#include "functions.h"
 
 #include "test_harness/test_harness.h"
 
@@ -11,7 +12,7 @@
 
 // Valeurs pour le harnais de test spécifiques à ce programme.
 // augmenter cette val à chaque test créer
-int const tests_total = 46;
+int const tests_total = 77;
 
 int const test_column_width = 80;
 
@@ -87,6 +88,10 @@ int main()
             TEST(get_first_id(begin(&restos), end(&restos)) == 4);
 
             TEST(ajout_resto("empburger", 82517, "06 53 12 89 75", "Fast Food", &restos) == 4);
+            TEST(ajout_resto("ALEXA donne un nom à mon resto jsp n'importe quoi", 83962, "06 89 48 35 36", "Italien", &restos) == -1);
+            TEST(ajout_resto("l'erreur du midi", 65471, "07 53 20 45 A6", "Bistro", &restos) == -2);
+            TEST(ajout_resto("japan service", 75000, "06 46 21 47 00", "B@r m@aid", &restos) == -3);
+            TEST(ajout_resto("suspect", 99254, "06 44 25 01 47 12", "Detective", &restos) == -2);
 
             TEST(size(restos) == 5);
 
@@ -147,6 +152,39 @@ int main()
 
             destroy(&menus);
         }
+    }
+
+    // test functions
+    {
+        TEST(isnom("Bar maid")==1);
+        TEST(isnom("")==0);
+        TEST(isnom("      ")==0);
+        TEST(isnom("AS  S")==0);
+        TEST(isnom("A S S")==1);
+        TEST(isnom("Sushi")==1);
+        TEST(isnom("pIzZA")==1);
+        TEST(isnom("poids chiche")==1);
+        TEST(isnom("B@r")==0);
+        TEST(isnom("P3t1t")==0);
+        TEST(isnom("yamana113")==0); // :(
+        TEST(isnom("l'entièreté des bouquin d'Harry Potter parcequ'ils sont trop longs")==0);
+        TEST(isnom("Joe s International House of Pancakes")==1);
+
+        TEST(isunder39("Joe's International House of Pancakes")==1);
+        TEST(isunder39("Jonny's International House of Pancakes")==1);
+        TEST(isunder39("Jonnathan's International House of Pancakes")==0);
+
+        TEST(istel("10 02 52 66 45")==1);
+        TEST(istel("07 52 14 89 63")==1);
+        TEST(istel("28 71 1 78 28 ")==0);
+        TEST(istel("18 174 15 01 4")==0);
+        TEST(istel("")==0);
+        TEST(istel("1")==0);
+        TEST(istel("dz ap os ke is")==0);
+        TEST(istel("14 55 01 4a 04")==0);
+        TEST(istel("@& _è *µ ^¨ çà")==0);
+        TEST(istel("14 25 36 87 69 ")==0);
+        TEST(istel("01 23 45 67 8")==0);
     }
 
     return tests_executed - tests_successful;
