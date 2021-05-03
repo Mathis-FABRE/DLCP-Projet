a093a010c4b7f444b6e4d04a8ea9177715d79ccc
yamana113 <mathisfabre2@gmail.com>
2021-05-03 13:02:24 +0200

    ajout commentaire db.h

--- lib/db.h
+++ lib/db.h
@@ -66,10 +66,35 @@ void ecriture_table(
 bool est_generaliste(
     void const *d);
 
+// fonction ajoutant un resto dans le vecteur resto
+// le vecteur est supposé trié en entrant et sortant
+// si la fonction marche correctement return > 0
+// return == id nouvel item
+// sinon return < 0
+// -1 : nom resto au dessus 39 char
+// -2 : tel invalide
+// -3 : type invalide
 int ajout_resto(char * nom, int code, char * telephone, char * type, vector * restos);
 
+// fonction ajoutant des ingredients dans un menu
+// ingredients sous forme
+// ingredient1;ingredient2;ingredien3=t3;...
+// renvoie 1 si fonction executer correctement
+// renvoie 0 si erreur
 int ajout_ingredients(char * ingredients, vector * v);
 
+// fonction ajoutant un menu dans le vecteur menu
+// le vecteur est supposé trié en entrant et sortant
+// si la fonction marche correctement return > 0
+// return == id nouvel item
+// sinon return < 0
+// -1 : nom resto au dessus 39 char
+// -2 : ingredients invalides
+// -3 : prix < 0
 int ajout_menu(char * nom, char * ingredients, float prix, vector * menus);
 
+// fonction ajoutant un menu dans un resto
+// si la fonction marche correctement return > 0
+// 1 : menu existe et rajouter correctement
+// 0 : id menu inexistant
 int menu_resto(size_t id, Restaurant * resto, vector const menus);
