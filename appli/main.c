d61ea5f8f41f7c961ccff900e3d83877b95ed693
yamana113 <mathisfabre2@gmail.com>
2021-05-17 23:44:24 +0200

    ajout présentation menu principale

--- appli/main.c
+++ appli/main.c
@@ -9,6 +9,24 @@
 #include <string.h>
 
 int main()
-{ 
+{
+    system("clear");
+    printf("#######################################\n");
+    printf(" Bienvenue sur kiki's delivery service \n");
+    printf("#######################################\n");
+    printf("\n");
+    printf("#######################################\n");
+    printf("             Menu Principal            \n");
+    printf("#######################################\n");
+    printf("\n");
+    printf("Vous êtes ?\n");
+    printf("1/ Un.e Restaurateur.tice\n");
+    printf("2/ Un.e Livreur.se\n");
+    printf("3/ Un.e Client.e\n");
+    printf("\n");
+    printf("Votre choix : ");
+
+    printf("\n");
+
     return 0;
 }
