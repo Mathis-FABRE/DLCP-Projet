0baedf5873a4c03e304efafc565c6c5451b94c34
yamana113 <mathisfabre2@gmail.com>
2021-05-17 23:00:59 +0200

    ajout bibliothèques nécessaires dans appli/main.c

--- appli/main.c
+++ appli/main.c
@@ -1,4 +1,14 @@
+#include "gestionfichier.h"
+#include "db.h"
+#include "algorithm.h"
+#include "functions.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int main()
-{
+{ 
     return 0;
 }
