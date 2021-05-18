02a96d94b40ab0458caa8d663d773d4798d40b06
yamana113 <mathisfabre2@gmail.com>
2021-05-18 22:28:49 +0200

    ajout menu client + resto + livreur

---
+++ lib/menu.c
@@ -0,0 +1,40 @@
+#include "menu.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void menu_client()
+{
+    system("clear");
+    printf("#######################################\n");
+    printf("         kiki's delivery sevice        \n");
+    printf("#######################################\n");
+    printf("\n");
+    printf("#######################################\n");
+    printf("              Menu Client              \n");
+    printf("#######################################\n");
+}
+
+void menu_resto()
+{
+    system("clear");
+    printf("#######################################\n");
+    printf("         kiki's delivery sevice        \n");
+    printf("#######################################\n");
+    printf("\n");
+    printf("#######################################\n");
+    printf("            Menu Restaurant            \n");
+    printf("#######################################\n");
+}
+
+void menu_livreur()
+{
+    system("clear");
+    printf("#######################################\n");
+    printf("         kiki's delivery sevice        \n");
+    printf("#######################################\n");
+    printf("\n");
+    printf("#######################################\n");
+    printf("              Menu Livreur             \n");
+    printf("#######################################\n");
+}
